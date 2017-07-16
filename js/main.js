619769ceea8ed5d7a4d5ed78893dc558e2fb9903
Gniewogniew <jakubbajer83@gmail.com>
2017-07-16 18:02:57 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$('#jabłko').click(function() {
+    $('<li class="new">').append("JABŁKO" + "<button class=delete>usuń</button>").prependTo('.items');
+});
+$('#gruszka').click(function() {
+    $('<li class="new">').append("GRUSZKA" + "<button class=delete>usuń</button>").prependTo('.items');
+});
+$('#banan').click(function() {
+    $('<li class="new">').append("BANAN" + "<button class=delete>usuń</button>").prependTo('.items');
+});
+$('#kiwi').click(function() {
+    $('<li class="new">').append("KIWI" + "<button class=delete>usuń</button>").prependTo('.items');
+});
+
+var $items = $(".items");
+$items.on("click", "li", "button", function() {
+    $(this).remove();
+
+});
+
+/* Funkcja do wiadomości */
+function msg() {
+
+    $("#input-msg").keypress(function(event) {
+        if (event.which == 13) {
+            $("#submit-btn").click();
+        }
+    });
+
+    $("#submit-btn").click(function() {
+        var message = $("#input-msg").val();
+        $("#msg").append("<p>" + message + "</p>");
+        $("#input-msg").val("");
+    });
+}
+$(document).ready(msg);
