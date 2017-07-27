ced8d6b0228b19df1e0508a1704b36e2849f983f
Gniewogniew <jakubbajer83@gmail.com>
2017-07-27 13:03:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,51 @@
 function attachEventHandlers (){
-  $(".products").click("click",(function(event){
-
-if (event.target.nodeName == "BUTTON"){
-console.log("Clicked", event.target.textContent);
-
-var productType = ("event.target.dataset.product");
-   
-var value = $("#" + productType + "-quantity")[0].value;
-
-       item=$("<li></li>"+"<button>Delete</button>")
-  item.append("event.target.dataset.product");
-  item.append("value");
-
-  item.appendTo($(".items"));
-
-};
-})) 
+  $(".products").click(productsClickHandler);
+  $(".items").click(basketClickHandler);
+  $("#message-button").click(newsClickHandler);
+  $("#message-input").keypress(inputTextClickHandler)
+  $(".products").click(translateProducts)
 }
+
+var productsClickHandler = function(event){
+    if(event.target.nodeName == "BUTTON") {
+    var productType = (event.target.dataset.product);   
+    var value = $("#" + productType + "-quantity")[0].value;
+
+       item = $("<li><button>Usuń</button></li>")
+       item.append(event.target.dataset.product + " ");
+       item.append(value);
+       item.appendTo($(".items"));
+    }
+}
+
+var basketClickHandler = function(event){
+    if(event.target.nodeName =="BUTTON"){
+    event.target.parentElement.remove();
+    }
+}
+
+var translateProducts = function(event){
+    if(event.target.nodeName == "BUTTON"){
+    "apple" : "Jabłka",
+    }
+}
+
+var newsClickHandler = function(event){
+    
+    var message = (event.target);
+    var inputText = (event.target.previousElementSibling).value;
+
+    item = $("<p>" +   + "</p>")
+    item.append(inputText);
+    item.appendTo($("#message"));
+    $("#message-input").val("");
+}
+
+var inputTextClickHandler = (function(event) {
+        if (event.which == 13) {
+            $("#message-button").click();
+
+    }
+});
 
 $(attachEventHandlers)
